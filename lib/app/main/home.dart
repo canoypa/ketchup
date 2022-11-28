f145a4a21c7b5130ce392ee25e200c7d0fd49b9a
Cano <contact.sasasaky@gmail.com>
2022-11-28 13:05:11 +0900

    make test widget

--- lib/app/main/home.dart
+++ lib/app/main/home.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ketchup/pomodoro/provider.dart';
 
 Page<dynamic> buildHomePage(context, state) {
   return const MaterialPage(
-    child: SafeArea(child: Text("home")),
+    child: SafeArea(child: PomodoroTest()),
   );
+}
+
+class PomodoroTest extends ConsumerWidget {
+  const PomodoroTest({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pomodoroNotifier = ref.watch(pomodoroTimerProvider.notifier);
+    final pomodoroState = ref.watch(pomodoroTimerProvider);
+
+    return Column(
+      children: [
+        Text("Status: ${pomodoroState.status.name}"),
+        OutlinedButton(
+          onPressed: () {
+            pomodoroNotifier.start();
+          },
+          child: const Text("START"),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            pomodoroNotifier.doneInterval();
+          },
+          child: const Text("DONE"),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            pomodoroNotifier.done();
+          },
+          child: const Text("DONE ALL"),
+        ),
+      ],
+    );
+  }
 }
