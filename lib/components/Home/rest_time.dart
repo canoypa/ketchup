0c1b1cca51cefdb5e66b085adc765c028815aeaa
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-11-29 11:57:56 +0900

    LayoutButtonで囲む

--- lib/components/Home/rest_time.dart
+++ lib/components/Home/rest_time.dart
@@ -7,32 +7,38 @@ class RestTime extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        SizedBox(
-          width: 140,
-          height: 50,
-          child: ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-              foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
-            ),
-            icon: const Icon(Icons.done_all),
-            label: const Text( '終了する',),
-            onPressed: () {},
-          ),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            return ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                foregroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+              icon: const Icon(Icons.done_all),
+              label: const Text(
+                '終了する',
+              ),
+              onPressed: () {},
+            );
+          },
         ),
         SizedBox(width: 8),
-        SizedBox(
-          width: 140,
-          height: 50,
-          child: ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-              foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
-            ),
-            icon: const Icon(Icons.play_arrow_outlined),
-            label: const Text( '再開する' ),
-            onPressed: () {},
-          ),
+                LayoutBuilder(
+          builder: (context, constraints) {
+            return ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                foregroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+              icon: const Icon(Icons.done_all),
+              label: const Text(
+                '再開する',
+              ),
+              onPressed: () {},
+            );
+          },
         )
       ],
     );
