8498a7c4281e59554d8a283ca0b38e51b309a9c3
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-12 14:12:54 +0900

    メモも全部受け取るクエリー書いた

--- lib/repository/pomodoro_memo.dart
+++ lib/repository/pomodoro_memo.dart
@@ -3,9 +3,13 @@ import 'package:ketchup/database/app_database.dart';
 class PomodoroMemoRepository {
   static  final _db = AppDatabase();
   
+  static Future intervalQuery(Map<String,dynamic> obj) async {
+    final db = await _db.database;
+    return await db.query('POMODORO_MEMO');
+  }
+
   static Future memoInsert(Map<String,dynamic> obj) async {
     final db = await _db.database;
-
     return (
       await db.insert(
         'POMODORO_MEMO',
