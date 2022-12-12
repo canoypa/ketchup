f7797a8259b9599d0f6bf4035df9ddc08354a900
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-12 12:07:36 +0900

    リポジトリにクエリーを移行した

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -53,35 +53,11 @@ class AppDatabase {
   }
 
   void _onCreate(Database db, int version) async {
-    /*
     await db.execute('''
     CREATE TABLE POMODORO (id TEXT PRIMARY KEY, title TEXT, category_id TEXT);
     CREATE TABLE POMODORO_INTERVAL (id TEXT PRIMARY KEY, pomodoro_id: TEXT, start_at: INTEGER, end_at: INTEGER);
     CREATE TABLE POMODORO_MEMO (id TEXT PRIMARY KEY, pomodoro_id: TEXT, value: TEXT, createdAt: INTEGER);
     CREATE TABLE CATEGORY (id TEXT PRIMARY KEY, title: TEXT, color: TEXT);
     ''');
-    */
-    await db.execute('''
-    CREATE TABLE POMODORO (id TEXT PRIMARY KEY, title TEXT, category_id TEXT);
-    ''');
-  }
-
-  Future runInsert(Database db,POMODORO pomodoro) async {
-    return (
-      db.insert(
-        'POMODORO',
-        pomodoro.toMap(),
-      )
-    );
-  }
-
-    Future runUpdate(Database db,POMODORO pomodoro) async {
-    return (
-      db.update(
-        'POMODORO',
-        pomodoro.toMap(),
-        where: 'id = ?',whereArgs: [pomodoro.pomodoro_id]
-      )
-    );
   }
 }
--- lib/repository/pomodoro.dart
+++ lib/repository/pomodoro.dart
@@ -1,1 +1,29 @@
-class PomodoroRepository {}
+import 'package:ketchup/database/app_database.dart';
+
+class PomodoroRepository {
+static  final _db = AppDatabase();
+
+  static Future runInsert(Map<String,dynamic> obj) async {
+    final db = await _db.database;
+
+    return (
+      await db.insert(
+        'POMODORO',
+        obj
+      )
+    );
+  }
+  //toMapした結果を受け取って、
+
+  static Future runUpdate(Map<String,dynamic>obj) async {
+    final db = await _db.database;
+
+    return (
+    await db.update(
+        'POMODORO',
+        obj,
+        where: 'id = ?',whereArgs: [obj["pomodoro_id"]]
+      )
+    );
+  }
+}
