5fe2d09dcd6748cb44fbe38ebd41baa3100d23aa
Cano <contact.sasasaky@gmail.com>
2022-12-12 14:14:19 +0900

    ライセンスページを作成 (#13)

--- lib/app/licenses.dart
+++ lib/app/licenses.dart
@@ -1,14 +1,69 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 Page<dynamic> buildLicensesPage(context, state) {
-  final l10n = L10n.of(context);
-
-  return MaterialPage(
+  return const MaterialPage(
     child: Scaffold(
       body: SafeArea(
-        child: Text(l10n.license),
+        child: MyLicensePage(),
       ),
     ),
   );
+}
+
+class MyLicensePage extends StatefulWidget {
+  const MyLicensePage({
+    super.key,
+  });
+
+  @override
+  MyLicensePageState createState() => MyLicensePageState();
+}
+
+class MyLicensePageState extends State<MyLicensePage> {
+  List<List<String>> licenses = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    LicenseRegistry.licenses.listen((l) {
+      final packageName = l.packages.toList().map((e) => e).join('');
+      final paragraphText = l.paragraphs.toList().map((e) => e.text).join('\n');
+
+      licenses.add([packageName, paragraphText]);
+      setState(() => licenses = licenses);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: licenses.length,
+        itemBuilder: (context, index) {
+          final license = licenses[index];
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  license[0],
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  license[1],
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
