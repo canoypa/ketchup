54bc1ca181275758273e69a6e490e4b0f2b54a3b
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-12 14:07:25 +0900

    idで絞り込んだcategory取得するクエリーを書いた

--- lib/repository/category.dart
+++ lib/repository/category.dart
@@ -5,7 +5,18 @@ class CategoryRepository {
 
   static Future categoryQuery(Map<String,dynamic> obj) async {
     final db = await _db.database;
+    //orderBy いるかも？
     return await db.query('CATEGORY');
+  }
+
+  static Future getCategory(Map<String,dynamic> obj) async {
+    final db = await _db.database;
+    return (
+      await db.query(
+        'CATEGORY',
+        where: 'id = ?',whereArgs: [obj["category_id"]]
+      )
+    );
   }
 
   static Future categoryInsert(Map<String,dynamic> obj) async {
@@ -17,7 +28,6 @@ class CategoryRepository {
       )
     );
   }
-  //toMapした結果を受け取って、
 
   static Future categoryUpdate(Map<String,dynamic>obj) async {
     final db = await _db.database;
@@ -26,8 +36,10 @@ class CategoryRepository {
     await db.update(
         'CATEGORY',
         obj,
-        where: 'id = ?',whereArgs: [obj["pomodoro_id"]]
+        where: 'id = ?',whereArgs: [obj["category_id"]]
       )
     );
   }
+
+  
 }
--- lib/repository/pomodoro_memo.dart
+++ lib/repository/pomodoro_memo.dart
@@ -1,8 +1,8 @@
 import 'package:ketchup/database/app_database.dart';
 
 class PomodoroMemoRepository {
-static  final _db = AppDatabase();
-
+  static  final _db = AppDatabase();
+  
   static Future memoInsert(Map<String,dynamic> obj) async {
     final db = await _db.database;
 
