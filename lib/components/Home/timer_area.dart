e06a351304cb4b9ef87090247a53dbb19d4f849c
kbc21a25 <kbc21@25@stu.kawahara.ac.jp>
2022-12-19 18:56:16 +0900

    Timer使って経過時間を表示したい

--- lib/components/Home/timer_area.dart
+++ lib/components/Home/timer_area.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:ketchup/components/Home/circular_progress.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ketchup/store/pomodoro/provider.dart';
 
 class TimerArea extends StatelessWidget {
   final Duration progressTime;
-  //
 
   const TimerArea({
     super.key,
@@ -14,16 +17,83 @@ class TimerArea extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       alignment: AlignmentDirectional.center,
-      children: [
-        const CircularProgress(),
-        Text(
-          '${progressTime.inSeconds}分',
-          style: TextStyle(
-            fontSize: 45,
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
-        ),
+      children: const [
+        CircularProgress(),
+        ProgressLabel(),
       ],
+    );
+  }
+}
+
+class ProgressLabel extends ConsumerStatefulWidget {
+  const ProgressLabel({Key? key}) : super(key: key);
+
+  @override
+  ProgressLabelState createState() => ProgressLabelState();
+}
+
+class ProgressLabelState extends ConsumerState<ProgressLabel> {
+  late DateTime _time;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _time = DateTime.now();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _time = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pomodoroState = ref.watch(pomodoroTimerProvider);
+    String dispTime = '';
+    DateTime startAt;
+    Duration intervalTime;
+    int diff;
+
+    startAt = (pomodoroState.interval != null)
+        ? pomodoroState.interval!.startAt
+        : DateTime.now();
+    intervalTime = (pomodoroState.interval != null)
+        ? pomodoroState.interval!.endAt
+            .difference(pomodoroState.interval!.startAt)
+        : const Duration(minutes: 25);
+
+    diff = _time.difference(startAt).inSeconds;
+    int hour = (diff / (60 * 60)).floor();
+    int mod = diff % (60 * 60);
+    int minutes = (mod / 60).floor();
+    int second = mod % 60;
+
+    if (hour != 0) {
+      dispTime += '$hour時間';
+    }
+    if (minutes != 0) {
+      dispTime += '$minutes分';
+    }
+    if (hour == 0 && minutes == 0 && second != 0) {
+      dispTime += '$second秒';
+    }
+    if (hour == 0 && minutes == 0 && second == 0) {
+      dispTime += '${intervalTime.inMinutes}分';
+    }
+
+    return Text(
+      dispTime,
+      style: TextStyle(
+        fontSize: 45,
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
     );
   }
 }
