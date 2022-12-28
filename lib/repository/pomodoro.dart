aa0968a83098c35a56ca90b0858c095ff5878ff0
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-28 15:03:53 +0900

    データベースの修正

--- lib/repository/pomodoro.dart
+++ lib/repository/pomodoro.dart
@@ -6,24 +6,36 @@ static  final _db = AppDatabase();
   static Future getData(String pomodoroId) async {
     final db = await _db.database;
     return (
-        await db.query(
+      await db.query(
         "pomodoro",
         where: 'id = ?',
         whereArgs: [pomodoroId],
       )
     );
   }
-
-static Future getCategoryData(String? category) async {
-  final db = await _db.database;
-  return (
+  
+  static Future getLimitData(String ) async {
+    final db = await _db.database;
+    return (
       await db.query(
         "pomodoro",
-        where: 'category_id = ?',
-        whereArgs: [category??"*"],
+        orderBy: 'createdAt',
+        limit: 2
       )
-  );
-}
+    );
+  }
+
+  static Future getCategoryData({String? category ,int? limit}) async {
+    final db = await _db.database;
+    return (
+        await db.query(
+          "pomodoro",
+          where: 'category_id = ?',
+          whereArgs: category !=null ? [category] : null,
+          limit: limit
+        )
+    );
+  }
 
   static Future runInsert(Map<String,dynamic> obj) async {
     final db = await _db.database;
