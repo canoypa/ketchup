ec44fd30711e38235819f52de895ca299bb4a787
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-23 12:46:33 +0900

    ダイアログをlistviewBuilderにした

--- lib/components/Home/timer_label.dart
+++ lib/components/Home/timer_label.dart
@@ -14,101 +14,131 @@ class TimerLabel extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 8),
-        InkWell(
-          onTap: () async {
-            final String? selectedText = await showDialog<String>(
-                context: context,
-                builder: (_) {
-                  return WillPopScope(
-                    child: const ChangeCategory(),
-                    onWillPop: () async => false,
-                  );
-                });
-          },
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            InkWell(
+              child: Icon(
                 Icons.circle,
                 color: Theme.of(context).colorScheme.outline,
               ),
-              Text(
+              onTap: () => {
+                // const ChangeColor(),
+              },
+            ),
+            InkWell(
+              onTap: () async {
+                final String? selectedText = await showDialog<String>(
+                  context: context,
+                  builder: (context) => _buildChoseCategoryDialog(context),
+                  );
+              },
+              child: Text(
                 'カテゴリ',
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.onSurfaceVariant,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        // const ChangeTitle(),
-        /*
-        ElevatedButton(
-          child: Text('Laravel'),
-          onPressed: () async {
-            final String? selectedText = await showDialog<String>(
-                context: context,
-                builder: (_) {
-                  return WillPopScope(
-                    child: const ChangeCategory(),
-                    onWillPop: () async => false,
-                  );
-                });
-          },
-        ),
-        */
         const SizedBox(height: 16),
       ],
     );
   }
 }
 
-class ChangeCategory extends StatefulWidget {
-  const ChangeCategory({super.key});
+Widget _buildChoseCategoryDialog(BuildContext context){
+  final List<String> categories = ["勉強", "読書", "音楽"];
+  
+  return SimpleDialog(
+      title: const Text('カテゴリーを選択'),
+      children: [
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.zero,
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            return SimpleDialogOption(
+              child: Text(categories[index]),
+              onPressed: () {
+                Navigator.pop(context, 'が選択されました');
+              },
+            );
+          },
+        ),
+        // SimpleDialogOption(
+        //   /*child: Row(
+        //     children: [
+        //       const TextField(
+        //         key: Key("uniqueKey"),
+        //         textAlign: TextAlign.center,
+        //         decoration: InputDecoration(
+        //           hintText: "カテゴリーを入力してください",
+        //           border: InputBorder.none,
+        //         ),
+        //       ),
+        //       ElevatedButton(
+        //         child: Text("追加"),
+        //         onPressed: () {
+        //           setState(() {});
+        //         },
+        //       ),
+        //     ],
+        //   ),*/
+        //   child: Text(""),
+        // ),
+      ],
+    );
+}
+/*
+class ChangeColor extends StatefulWidget {
+  const ChangeColor({super.key});
 
   @override
-  State<ChangeCategory> createState() => _ChangeCategoryState();
+  State<ChangeColor> createState() => _ChangeColorState();
 }
 
-class _ChangeCategoryState extends State<ChangeCategory> {
-  final List<String> categories = ["勉強", "読書", "音楽"];
+class _ChangeColorState extends State<ChangeColor> {
+  // create some values
+  Color pickerColor = Color(0xff443a49);
+  Color currentColor = Color(0xff443a49);
+  void changeColor(Color color) {
+    setState(() => pickerColor = color);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      title: const Text('カテゴリーを選択'),
-      children: [
-        /*
-        Expanded(
-          child: ListView(
-            children: categories.map((category) {
-              return SimpleDialogOption(
-                child: Text(category),
-                onPressed: () {
-                  Navigator.pop(context, 'が選択されました');
-                },
-              );
-            }).toList(),
-          ),
-        ),
-        */
-        Row(children: [
-          const TextField(
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-              hintText: "カテゴリーを入力してください",
-              border: InputBorder.none,
+    return FutureBuilder(
+      future: showDialog(
+        builder: (context) => AlertDialog(
+          title: const Text('Pick a color!'),
+          content: SingleChildScrollView(
+            child: MultipleChoiceBlockPicker(
+              pickerColors: currentColors,
+              onColorsChanged: changeColors,
             ),
           ),
-          ElevatedButton(
-            child: Text("追加"),
-            onPressed: () {
-              
-            },
-          ),
-        ]),
-      ],
+          actions: [
+            ElevatedButton(
+              child: const Text('Got it'),
+              onPressed: () {
+                setState(() => currentColor = pickerColor);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ), context: context,
+      ),
+      builder: (context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          return Container(); // replace this with the widget you want to show after the dialog is closed
+        } else {
+          return Container(); // replace this with the widget you want to show while the dialog is open
+        }
+      },
     );
   }
 }
+*/
