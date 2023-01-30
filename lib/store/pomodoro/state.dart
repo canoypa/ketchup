096593302daad2967350f0a59ff904f81923948f
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-30 14:59:08 +0900

    ステート変更

--- lib/store/pomodoro/state.dart
+++ lib/store/pomodoro/state.dart
@@ -29,8 +29,8 @@ class PomodoroMeasureNotifier extends StateNotifier<PomodoroMeasure> {
   }) {
     state = state.maybeMap(
       waiting: (value) => value.copyWith.info(
-        title: title,
-        categoryId: categoryId,
+        title: title??value.info.title,
+        categoryId: categoryId??value.info.categoryId,
       ),
       orElse: () => throw Error(),
     );
